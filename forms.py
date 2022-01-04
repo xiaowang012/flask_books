04e7ef64dcc8b5dd00545b043ebffdc196dabb82
xiaowang012 <1300202481@qq.com>
2022-01-04 16:31:12 +0800

    添加了批量导入表单

--- forms.py
+++ forms.py
@@ -4,12 +4,14 @@ from wtforms import StringField,PasswordField,SelectField,DateField,FloatField,S
 from wtforms.validators import AnyOf, DataRequired, EqualTo,Length, NumberRange
 from flask_wtf.file import FileRequired,FileAllowed
 
+#用户注册表单
 class RegisterForms(FlaskForm):
     username = StringField('username',validators = [DataRequired('Username cannot be empty!'),Length(min=6,max=12,message = 'The username must be 6-12 characters long!')])
     password = PasswordField ('password',validators = [DataRequired('Password cannot be empty!'),Length(min=6,max=12,message = 'The password must be 6-12 characters long!')])
     password1 = PasswordField ('password1',validators = [DataRequired('Password1 cannot be empty!'),Length(min=6,max=12,message = 'The password must be 6-12 characters long!'),EqualTo('password',message = 'The entered passwords are inconsistent')])
     submit = SubmitField('submit')
 
+#用户登录表单
 class UserForms(FlaskForm):
     username = StringField('username',validators = [DataRequired('Username cannot be empty!'),Length(min=6,max=12,message = 'The username must be 6-12 characters long!')])
     password = PasswordField ('password',validators = [DataRequired('Password cannot be empty!'),Length(min=6,max=12,message = 'The password must be 6-12 characters long!')])
@@ -33,12 +35,12 @@ class StudentsInfoForms(FlaskForm):
     hobbies = StringField('hobbies',validators = [DataRequired('兴趣爱好不能为空!')])
     submit = SubmitField('提交')
 
+#home界面查询书本的表单
 class SearchIdForms(FlaskForm):
-    #home页面的输入框
     book_name= StringField('book_name',validators = [DataRequired()])
     submit = SubmitField('submit')
 
+#批量导入用户的表单
 class UploadFileForms(FlaskForm):
-    #批量导入页面过滤其他文件类型
     file = FileField('file',validators = [FileRequired(message = 'File cannot be empty!'),FileAllowed(['xlsx','xls'],message = 'File format error (XLSX/XLS only)!')])
     submit = SubmitField('submit')
