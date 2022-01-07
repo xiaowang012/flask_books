3c6034a3371d47a0be2a9cd206868737d2d6c489
xiaowang012 <1300202481@qq.com>
2022-01-07 10:35:04 +0800

    新增了权限表为缓存方式

--- decorator.py
+++ decorator.py
@@ -4,6 +4,10 @@ from functools import wraps
 from models import Permission, User,UserGroup
 import hashlib
 
+
+# # 权限缓存
+PERMISSION_DICT = {}
+
 #检查登录
 def login_required(func):
     @wraps(func) 
@@ -34,18 +38,15 @@ def routing_permission_check(func):
         #获取当前访问的url
         current_url = str(request.full_path).rstrip('?')
 
-        result = User.query.filter(User.username == user_id).first()
-        if result:
-            group_id = result.group_id
-            result1 = UserGroup.query.filter(UserGroup.id == group_id).first()
-            if result1:
-                name = result1.name
-                result2 = Permission.query.filter(Permission.name == name).all()
-                url_list = []
-                if len(result2) != 0:
-                    for per in result2:
-                        url_list.append(per.url)
-                    if current_url in url_list:
+        #判断权限字典是否为空
+        if PERMISSION_DICT:
+            result = User.query.filter(User.username == user_id).first()
+            if result:
+                group_id = result.group_id
+                result1 = UserGroup.query.filter(UserGroup.id == group_id).first()
+                if result1:
+                    name = result1.name
+                    if current_url in PERMISSION_DICT[name]:
                         return func(*args,**kwargs)
                     else:
                         return jsonify({'code':403,'message':'Unauthorized access'})
@@ -54,7 +55,62 @@ def routing_permission_check(func):
             else:
                 return jsonify({'code':403,'message':'Unauthorized access'})
         else:
-            return jsonify({'code':403,'message':'Unauthorized access'})
+            #在全局变量中写入权限表中得数据格式为{'admin':{},'others':{}}
+            #查询所有的用户组
+            user_group_list = []
+            user_group_data = UserGroup.query.all()
+            if user_group_data:
+                for i in user_group_data:
+                    user_group_list.append( i.name)
+                for j in user_group_list:
+                    result2 = Permission.query.filter(Permission.name == j).all()
+                    set1 = set()
+                    if result2:
+                        for k in result2:
+                            set1.add(k.url)
+                        PERMISSION_DICT[j] = set1
+                    else:
+                        return jsonify({'code':403,'message':'Unauthorized access'})
+            else:
+                return jsonify({'code':403,'message':'Unauthorized access'})
+            # print(PERMISSION_DICT)
+            result = User.query.filter(User.username == user_id).first()
+            if result:
+                group_id = result.group_id
+                result1 = UserGroup.query.filter(UserGroup.id == group_id).first()
+                if result1:
+                    name = result1.name
+                    if current_url in PERMISSION_DICT[name]:
+                        return func(*args,**kwargs)
+                    else:
+                        return jsonify({'code':403,'message':'Unauthorized access'})
+                else:
+                    return jsonify({'code':403,'message':'Unauthorized access'})
+            else:
+                return jsonify({'code':403,'message':'Unauthorized access'})
+
+        # result = User.query.filter(User.username == user_id).first()
+        # if result:
+        #     group_id = result.group_id
+        #     result1 = UserGroup.query.filter(UserGroup.id == group_id).first()
+        #     if result1:
+        #         name = result1.name
+        #         result2 = Permission.query.filter(Permission.name == name).all()
+        #         url_list = []
+
+        #         if len(result2) != 0:
+        #             for per in result2:
+        #                 url_list.append(per.url)
+        #             if current_url in url_list:
+        #                 return func(*args,**kwargs)
+        #             else:
+        #                 return jsonify({'code':403,'message':'Unauthorized access'})
+        #         else:
+        #             return jsonify({'code':403,'message':'Unauthorized access'})
+        #     else:
+        #         return jsonify({'code':403,'message':'Unauthorized access'})
+        # else:
+        #     return jsonify({'code':403,'message':'Unauthorized access'})
     return wrapper
 
 #hash加密
@@ -63,3 +119,5 @@ def get_hash_value(pwd,salt):
     hash.update(pwd.encode('utf-8'))
     hash_value = hash.hexdigest()
     return hash_value
+
+
