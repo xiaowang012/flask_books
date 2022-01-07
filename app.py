738c7e4ac303c727a3429034c3b76676cb71df69
xiaowang012 <1300202481@qq.com>
2022-01-07 10:37:19 +0800

    修改host为0.0.0.0

--- app.py
+++ app.py
@@ -816,4 +816,4 @@ def book_page():
     return render_template('book.html')
 
 if __name__ == '__main__':
-    app.run(host = '127.0.0.1',port=5000,debug = True)
+    app.run(host = '0.0.0.0',port=5000,debug = True)
